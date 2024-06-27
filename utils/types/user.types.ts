2695569e223e9238957ea044ab47a0b376794845
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-06-27 23:44:55 +0900

    feat: usertype (#490)

---
+++ utils/types/user.types.ts
@@ -0,0 +1,14 @@
+export interface UserProfileDataType {
+  userId: number;
+  userName: string;
+  profileImage: string;
+  followerCount: number;
+  followingCount: number;
+  height: number;
+  weight: number;
+  description: string;
+  isMyProfile: Boolean;
+  isFollow: Boolean;
+  ootdCount: number;
+  clothesCount: number;
+}
