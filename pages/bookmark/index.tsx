d6773f6773a0cdd2c56d5de61d986aaaf71b09a3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-07-19 23:30:56 +0900

    feat: toast와 fab 관계 설정

--- pages/bookmark/index.tsx
+++ pages/bookmark/index.tsx
@@ -198,6 +198,7 @@ export default function Bookmark() {
             text="북마크에서 삭제되었습니다."
             setState={setToastOpen}
             state={toastOpen}
+            alertState={isVisible}
           />
         )}
       </S.Layout>
