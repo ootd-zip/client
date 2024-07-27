ea02cf78865810fa717dac4f8c9486330fc37a2d
skrgus6269 <knh6269@gmail.com>
2024-07-28 01:58:06 +0900

    feat: 선택 버튼 인풋 색상 수정 (#519)

--- components/Input/TrueFalse/style.tsx
+++ components/Input/TrueFalse/style.tsx
@@ -12,20 +12,28 @@ interface ButtonProps {
 }
 
 const LeftButton = styled.button<ButtonProps>`
-  background-color: ${(props) => (props.state ? 'black' : 'white')};
+  background-color: ${(props) =>
+    props.state ? props.theme.color.blue_100 : 'white'};
   border-radius: 2px 0 0 2px;
-  color: ${(props) => (props.state ? 'white' : 'black')};
+  color: ${(props) =>
+    props.state ? props.theme.color.blue_500 : props.theme.color.grey_50};
   width: 50%;
-  border: 1px solid black;
-  transition: 0.3s;
+  border: 1px solid
+    ${(props) =>
+      props.state ? props.theme.color.blue_500 : props.theme.color.grey_90};
+  border-right: ${(props) => !props.state && 'none'};
 `;
 const RightButton = styled.button<ButtonProps>`
-  background-color: ${(props) => (props.state ? 'white' : 'black')};
-  color: ${(props) => (props.state ? 'black' : 'white')};
+  background-color: ${(props) =>
+    !props.state ? props.theme.color.blue_100 : 'white'};
+  color: ${(props) =>
+    !props.state ? props.theme.color.blue_500 : props.theme.color.grey_50};
   width: 50%;
-  transition: 0.3s;
   border-radius: 0 2px 2px 0;
-  border: 1px solid black;
+  border: 1px solid
+    ${(props) =>
+      !props.state ? props.theme.color.blue_500 : props.theme.color.grey_90};
+  border-left: ${(props) => props.state && 'none'};
 `;
 
 const S = { Layout, LeftButton, RightButton };
