1a84414eee69a208ae0138dc5561c7de453041ce
JungYeonHwi <wjddusgnl676@naver.com>
2024-11-16 23:38:22 +0900

    feat: 설정 브랜드 건의 페이지 구성

---
+++ pageStyle/brand-suggestion/style.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  .arrowleft,
+  .setting {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+const Content = styled.div`
+  margin-top: 32px;
+  padding: 0px 20px;
+
+  .warning {
+    margin: 8px 0px 24px 0px;
+  }
+`;
+
+interface ButtonProps {
+  state: Boolean;
+}
+
+const Button = styled.div<ButtonProps>`
+  margin: 0px 20px;
+  width: calc(100% - 40px);
+  position: fixed;
+  bottom: 24px;
+  background-color: ${(props) =>
+    props.state ? props.theme.color.grey_00 : props.theme.color.grey_90};
+  padding: 14px 0px;
+  color: #fff;
+  text-align: center;
+`;
+
+const S = {
+  Layout,
+  Content,
+  Button,
+};
+
+export default S;
